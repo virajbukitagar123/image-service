df317a175059f98ec19fc50d59a1228815a2918c
virajbukitagar123 <viraj.bukitagar@gmail.com>
2024-02-18 21:45:42 +0530

    Adding CORS header.

--- src/main/java/com/chess/imageservice/GifStoreController.java
+++ src/main/java/com/chess/imageservice/GifStoreController.java
@@ -4,6 +4,7 @@ import com.chess.imageservice.service.ObjectStoreService;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,7 @@ public class GifStoreController {
         this.objectStoreService = objectStoreService;
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/{openingName}/{move}")
     public ResponseEntity<Resource> returnGif(@PathVariable String openingName, @PathVariable int move, HttpServletResponse response) throws IOException {
         Optional<File> fileOp = objectStoreService.getGif(openingName, move);
